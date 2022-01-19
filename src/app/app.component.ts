6764dd21fc74d9db66650523c0b999e12b5c1472
kirana123 <kirana@techfino.in>
2022-01-19 19:29:46 +0530

    assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'assignment';
+
+  constructor(
+    // Angular Modules 
+    private http: HttpClient,
+    private userService: UserService,
+  ) { }
+
+  ngOnInit() {
+    this.userService.getBrewers().subscribe((res: any) => {
+      console.log(res)
+    });
+  }
 }
---
+++ src/app/user/user.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
+
+  getBrewers(){
+    return this.http.get('https://api.openbrewerydb.org/breweries');
+  }
+}
